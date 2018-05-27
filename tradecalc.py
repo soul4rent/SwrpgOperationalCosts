927a196d5aa6b0c75beb982db2c7052a89e29abe
soul4rent <kyle.a.wadsworth@gmail.com>
2018-05-27 11:23:52 -0700

    minor updates

--- tradecalc.py
+++ tradecalc.py
@@ -8,6 +8,13 @@ price = 0
 rarity = 0
 quantity = 0
 
+#changes price based on check
+checkSuccesses = 0
+checkAdvangates = 0
+
+#from difficulty table
+difficulty = 0 #TODO: CHANGE LATER
+
  
 #tk window stuff
 window = Tk()
@@ -30,11 +37,13 @@ rarityEntry = Entry(window, width=10)
 rarityEntry.grid(column=1, row=1)
 
 def TradeCalc():
-    print("test")
+    price = float(priceEntry.get())
+    rarity = int(rarityEntry.get())
+    print("Price:" + str(price) + " Rarity:" + str(rarity))
 
 
 calculateBtn = Button(window, text="Calculate", command=TradeCalc)
-calculateBtn.grid(column=2, row=0)
+calculateBtn.grid(column=0, row=5)
 
  
 window.mainloop()
